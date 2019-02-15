e29bf3d988775fdd1eef6fcccd2f6318ab445b2d
EC2 Default User <ec2-user@ip-172-31-34-244.ap-southeast-1.compute.internal>
2019-02-15 09:10:13 +0000

    投稿ページのビューのエラーを修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,13 @@ class TweetsController < ApplicationController
   
   def new
   end
+  
+  def create
+    @tweet = Tweet.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.permit(:text)
+  end
 end
